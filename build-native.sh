4d94b371974d82b8ceee82ad9d199d15d3324635
Hazel Stagner <glairedaggers@gmail.com>
2023-10-20 14:06:55 -0700

    Fixed build-native.sh

--- build-native.sh
+++ build-native.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 scriptPath="`dirname \"$0\"`"
-cimguiPath=$scriptPath/cimgui
 
 _CMakeBuildType=Debug
 _CMakeOsxArchitectures=
@@ -30,8 +29,8 @@ while :; do
     shift
 done
 
-mkdir -p $cimguiPath/build/$_CMakeBuildType
-pushd $cimguiPath/build/$_CMakeBuildType
+mkdir -p $scriptPath/build/$_CMakeBuildType
+pushd $scriptPath/build/$_CMakeBuildType
 cmake ../.. -DCMAKE_OSX_ARCHITECTURES="$_CMakeOsxArchitectures" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.13 -DCMAKE_BUILD_TYPE=$_CMakeBuildType
 make
 popd
